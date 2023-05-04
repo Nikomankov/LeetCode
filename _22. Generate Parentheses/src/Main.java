7610154be1d9aac592bece294d1811dee23f7723
Nikita Mankov <kislobaa@gmail.com>
2023-05-04 12:58:52 +0700

    22. Generate Parentheses

---
+++ _22. Generate Parentheses/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 3;
+        Solution.generateParenthesis(n).forEach(e -> System.out.println(e + ", "));
+
+
+    }
+}
+
+
+class Solution {
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        recursiveGenerator(result, "", n, n);
+        return result;
+    }
+
+    public static void recursiveGenerator(List<String> result, String s, int left, int right){
+        if(left == 0 && right == 0){
+            result.add(s);
+            return;
+        }
+        if(left > 0){
+            recursiveGenerator(result, s + "(", left - 1, right);
+        }
+        if(right > left){
+            recursiveGenerator(result, s + ")", left, right - 1);
+        }
+    }
+}
